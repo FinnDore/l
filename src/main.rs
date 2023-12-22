72c27536239c0e6fb1883aab4ce8f35a1a0afb44
Finn <34718806+FinnDore@users.noreply.github.com>
2023-12-22 22:29:17 +0000

    🪵

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{self, BufRead};
+use std::process::Command;
+
+use chrono::Utc;
 fn main() {
-    println!("🤮");
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock(); // locking is optional
+
+    let mut line = String::new();
+
+    while let Ok(n_bytes) = stdin.read_line(&mut line) {
+        if n_bytes == 0 {
+            break;
+        }
+
+        print!("{}", line);
+        Command::new("src/write-log.appl")
+            .arg(format!(
+                "{} {}",
+                Utc::now().format("%Y-%m-%dT%H:%M:%S"),
+                line.to_string()
+            ))
+            .output()
+            .expect("Failed to execute command");
+        line.clear()
+    }
 }
